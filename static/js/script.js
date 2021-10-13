de97f3d85cd424e5a356b0571139a020e237e665
Richad MERALLI <richad.meralli@gmail.com>
2021-10-13 21:07:37 +0200

    added increasing animation for numbers

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,29 @@
+var speed = 1;
 
+/* Call this function with a string containing the ID name to
+ * the element containing the number you want to do a count animation on.*/
+function incEltNbr(id) {
+  number = document.getElementById(id);
+  finalNumber = Number(document.getElementById(id).innerHTML);
+  incNbrRec(0, finalNumber, number);
+}
+
+/*A recursive function to increase the number.*/
+function incNbrRec(i, finalNumber, number) {
+  if (i <= finalNumber) {
+    number.innerHTML = i;
+    setTimeout(function () {
+      //Delay a bit before calling the function again.
+      incNbrRec(i + 1, finalNumber, number);
+    }, speed);
+  }
+}
+
+/*Function called on button click*/
+function incNbr() {
+  incEltNbr("nbr");
+}
+
+incEltNbr(
+  "nbr-quotes"
+); /*Call this funtion with the ID-name for that element to increase the number within*/
